1e489f7cdfab615051165ee045eab136bf88ed1a
walter <walter-zero@hotmail.com>
2021-04-07 01:42:31 -0300

    #Added login to settings

--- app/src/main/java/com/example/android/firebaseui_login_sample/LoginFragment.kt
+++ app/src/main/java/com/example/android/firebaseui_login_sample/LoginFragment.kt
@@ -65,6 +65,24 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Observe the authentication state so we can know if the user has logged in successfully.
+// If the user has logged in successfully, bring them back to the settings screen.
+// If the user did not log in successfully, display an error message.
+        viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+            when (authenticationState) {
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> navController.popBackStack()
+                else -> Log.e(
+                    TAG,
+                    "Authentication state that doesn't require any UI change $authenticationState"
+                )
+            }
+        })
+
+        // If the user presses the back button, bring them back to the home screen
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            navController.popBackStack(R.id.mainFragment, false)
+        }
+
         navController = findNavController()
     }
 
